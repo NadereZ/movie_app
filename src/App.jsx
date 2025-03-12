82770f4d484315eff889b21c00765edbd1bb321d
nadere <naderezaboli@gmail.com>
2025-03-12 15:35:21 +0330

    create pages folder and Home and Favorite files.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import MovieCard from './components/MovieCard'
+
+function App() {
+ 
+  return (
+    <>
+    <div>
+      <MovieCard movie={{title:'abc', release_date:'2006'}}/>
+    </div>
+    </>
+  )
+}
+
+export default App
